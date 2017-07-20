9e1eb0a15de6bdc9602f67e2e7fd159bc47e426b
鹿野壮 <takeshi.kano@ics-web.jp>
2017-07-20 18:12:46 +0900

    Sassコンパイルタスクの追加

---
+++ sass_task/myproject/gulpfile.js
@@ -0,0 +1,14 @@
+/**
+ * Sassのコンパイルタスクのサンプルファイルです。
+ */
+// gulpプラグインの読み込み
+var gulp = require("gulp");
+// Sassをコンパイルするプラグインの読み込み
+var sass = require("gulp-sass");
+
+// style.scssをタスクを作成する
+gulp.task("default", function () {
+  gulp.src("css/style.scss")  // style.scssファイルを取得
+    .pipe(sass())  // Sassのコンパイルを実行
+    .pipe(gulp.dest("css"));  // cssフォルダー以下に保存
+});
