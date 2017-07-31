03b8f4f53b28340d4db0ebbb5cbdffe3c1a3065d
鹿野壮 <takeshi.kano@ics-web.jp>
2017-07-31 17:11:10 +0900

    Sassコンパイル時の整形

--- sass_task/myproject/gulpfile.js
+++ sass_task/myproject/gulpfile.js
@@ -9,6 +9,8 @@ var sass = require("gulp-sass");
 // style.scssをタスクを作成する
 gulp.task("default", function () {
   gulp.src("css/style.scss")  // style.scssファイルを取得
-    .pipe(sass())  // Sassのコンパイルを実行
+    .pipe(sass({
+      outputStyle: "expanded"
+    }))  // Sassのコンパイルを実行
     .pipe(gulp.dest("css"));  // cssフォルダー以下に保存
 });
