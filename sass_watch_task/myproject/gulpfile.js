ff3237caf4e060346b3f5ad60ad7297cf6613aa7
ics-ikeda <ikeda@ics-web.jp>
2017-08-22 18:50:49 +0900

    コードフォーマット

--- sass_watch_task/myproject/gulpfile.js
+++ sass_watch_task/myproject/gulpfile.js
@@ -1,19 +1,23 @@
-/**
- * Sassのコンパイルタスクのサンプルファイルです。
- */
+// Sassのコンパイルタスクのサンプルファイルです。
+
 // gulpプラグインの読み込み
-var gulp = require("gulp");
+const gulp = require('gulp');
 // Sassをコンパイルするプラグインの読み込み
-var sass = require("gulp-sass");
+const sass = require('gulp-sass');
 
 // style.scssの監視タスクを作成する
-gulp.task("default", function () {
-  gulp.watch("css/style.scss", function () {  // style.scssファイルを監視
-    // style.scssの更新があったら、style.scssをコンパイルする
-    gulp.src("css/style.scss")  // style.scssファイルを取得
+gulp.task('default', function () {
+  // style.scssファイルを監視
+  gulp.watch('css/style.scss', function () {
+    // style.scssの更新があった場合の処理
+
+    // style.scssファイルを取得
+    gulp.src('css/style.scss')
+    // Sassのコンパイルを実行
       .pipe(sass({
-        outputStyle: "expanded"
-      }))  // Sassのコンパイルを実行
-      .pipe(gulp.dest("css"));  // cssフォルダー以下に保存
+        outputStyle: 'expanded'
+      }))
+      // cssフォルダー以下に保存
+      .pipe(gulp.dest('css'));
   });
 });
