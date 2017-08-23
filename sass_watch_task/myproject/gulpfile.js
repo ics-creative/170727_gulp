d2116f2e46cbb5c079e696708888611563d4b176
ics-ikeda <ikeda@ics-web.jp>
2017-08-23 12:49:47 +0900

    watchコマンドのエラーを防止

--- sass_watch_task/myproject/gulpfile.js
+++ sass_watch_task/myproject/gulpfile.js
@@ -7,16 +7,19 @@ const sass = require('gulp-sass');
 
 // style.scssの監視タスクを作成する
 gulp.task('default', function () {
-  // style.scssファイルを監視
+  // ★ style.scssファイルを監視
   gulp.watch('css/style.scss', function () {
     // style.scssの更新があった場合の処理
 
     // style.scssファイルを取得
     gulp.src('css/style.scss')
-    // Sassのコンパイルを実行
+      // Sassのコンパイルを実行
       .pipe(sass({
         outputStyle: 'expanded'
-      }))
+      })
+      // Sassのコンパイルエラーを表示
+      // (これがないと自動的に止まってしまう)
+        .on('error', sass.logError))
       // cssフォルダー以下に保存
       .pipe(gulp.dest('css'));
   });
