40ffff445b8ca4ee6f54a1d65f09f9f9dbc4af6d
ics-ikeda <ikeda@ics-web.jp>
2022-05-24 19:09:52 +0900

    prettierでコードフォーマット

--- sass_watch_task/myproject/gulpfile.js
+++ sass_watch_task/myproject/gulpfile.js
@@ -1,26 +1,31 @@
 // Sassのコンパイルタスクのサンプルファイルです。
 
 // gulpプラグインの読み込み
-const gulp = require('gulp');
+const gulp = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
-const sass = require('gulp-sass')(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
 
 // style.scssの監視タスクを作成する
-gulp.task('default', () => {
+gulp.task("default", () => {
   // ★ style.scssファイルを監視
-  return gulp.watch('css/style.scss', () => {
+  return gulp.watch("css/style.scss", () => {
     // style.scssの更新があった場合の処理
 
     // style.scssファイルを取得
-    return gulp.src('css/style.scss')
-      // Sassのコンパイルを実行
-      .pipe(sass({
-        outputStyle: 'expanded'
-      })
-      // Sassのコンパイルエラーを表示
-      // (これがないと自動的に止まってしまう)
-      .on('error', sass.logError))
-      // cssフォルダー以下に保存
-      .pipe(gulp.dest('css'));
+    return (
+      gulp
+        .src("css/style.scss")
+        // Sassのコンパイルを実行
+        .pipe(
+          sass({
+            outputStyle: "expanded",
+          })
+            // Sassのコンパイルエラーを表示
+            // (これがないと自動的に止まってしまう)
+            .on("error", sass.logError)
+        )
+        // cssフォルダー以下に保存
+        .pipe(gulp.dest("css"))
+    );
   });
 });
