c244c6664957f5569cfc6cc391e46358262a5436
ics-ikeda <ikeda@ics-web.jp>
2026-05-15 14:25:16 +0900

    Gulp関連モジュールとタスク定義を更新

--- sass_task/myproject/gulpfile.js
+++ sass_task/myproject/gulpfile.js
@@ -1,16 +1,22 @@
 // gulpプラグインの読み込み
-const gulp = require('gulp');
+const { src, dest } = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
-const sass = require('gulp-sass')(require('sass'));
+const sass = require("gulp-sass")(require("sass"));
 
 // style.scssをタスクを作成する
-gulp.task('default',  () => {
+function defaultTask() {
   // style.scssファイルを取得
-  return gulp.src('css/style.scss')
-  // Sassのコンパイルを実行
-    .pipe(sass({
-      outputStyle: 'expanded'
-    }))
-    // cssフォルダー以下に保存
-    .pipe(gulp.dest('css'));
-});
+  return (
+    src("css/style.scss")
+      // Sassのコンパイルを実行
+      .pipe(
+        sass({
+          style: "expanded"
+        })
+      )
+      // cssフォルダー以下に保存
+      .pipe(dest("css"))
+  );
+}
+
+exports.default = defaultTask;
--- sass_watch_task/myproject/gulpfile.js
+++ sass_watch_task/myproject/gulpfile.js
@@ -1,31 +1,30 @@
-// Sassのコンパイルタスクのサンプルファイルです。
-
 // gulpプラグインの読み込み
-const gulp = require("gulp");
+const { src, dest, watch } = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass")(require("sass"));
 
-// style.scssの監視タスクを作成する
-gulp.task("default", () => {
-  // ★ style.scssファイルを監視
-  return gulp.watch("css/style.scss", () => {
-    // style.scssの更新があった場合の処理
+function compileStyles() {
+  // style.scssファイルを取得
+  return (
+    src("css/style.scss")
+      // Sassのコンパイルを実行
+      .pipe(
+        sass({
+          style: "expanded",
+        })
+        // Sassのコンパイルエラーを表示
+        // (これがないと自動的に止まってしまう)
+        .on("error", sass.logError)
+      )
+      // cssフォルダー以下に保存
+      .pipe(dest("css"))
+  );
+}
 
-    // style.scssファイルを取得
-    return (
-      gulp
-        .src("css/style.scss")
-        // Sassのコンパイルを実行
-        .pipe(
-          sass({
-            outputStyle: "expanded",
-          })
-            // Sassのコンパイルエラーを表示
-            // (これがないと自動的に止まってしまう)
-            .on("error", sass.logError)
-        )
-        // cssフォルダー以下に保存
-        .pipe(gulp.dest("css"))
-    );
-  });
-});
+// style.scssの監視タスクを作成する
+function defaultTask() {
+  // ★ style.scssファイルを監視
+  return watch("css/style.scss", compileStyles);
+}
+
+exports.default = defaultTask;
